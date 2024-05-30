56b194416e791d2e115df48450247c41352bbc7c
Alex Kruglov <99730932+JeFFich@users.noreply.github.com>
2024-05-30 15:45:22 +0300

    Update preprocessing.py

--- app/src/preprocessing.py
+++ app/src/preprocessing.py
@@ -4,22 +4,20 @@ from category_encoders import TargetEncoder
 path_data = './input/'
 train = pd.read_csv(path_data + 'train.csv')
 
+# Кодирование категориального через terget encoding
 Targetenc = TargetEncoder().fit(X=train.pack, y=train.binary_target)
-target_enc_df = Targetenc.transform(X=train["pack"])
-target_enc_df["pack_name"] = train.pack
 
-cols = ["pack", "сумма", "on_net", "секретный_скор"]
-
+# Кодирование числовых признаков через медианы
 features_medians = {"сумма": 0, "on_net": 0, "секретный_скор": 0}
-
 for feature in ["сумма", "on_net", "секретный_скор"]:
     # запомним медианы
     features_medians[feature] = train[feature].median()
 
 
 def preprocess(save_location: str) -> pd.DataFrame:
-    """предобрабатывает входные данные"""
+    """Предобработка входных данных"""
     test = pd.read_csv(save_location)
+    
     for feature in ["сумма", "on_net", "секретный_скор"]:
         test[feature].fillna(features_medians[feature], inplace=True)
     test['pack'] = Targetenc.transform(X=test.pack)
